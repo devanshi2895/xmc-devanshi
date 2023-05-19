7502577a9a315c553909fb99c3c92292d9844be8
Devanshi <dkakade@horizontal.com>
2023-05-19 14:59:02 +0530

    committing site A items

--- src/sxastarter/src/components/FeatureBlogCR.tsx
+++ src/sxastarter/src/components/FeatureBlogCR.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Field } from '@sitecore-jss/sitecore-jss-nextjs';
+import { Field, Text } from '@sitecore-jss/sitecore-jss-nextjs';
 
 interface Fields {
   items: {
@@ -30,27 +30,32 @@ const FeatureBlogDefaultComponent = (): JSX.Element => (
 //https://andypaz.com/2020/10/21/creating-a-custom-rendering-contents-resolver/
 
 export const Default = (props: FeatureBlogProps): JSX.Element => {
-  //   const ds = props.fields.data.datasource;
+  const ds = props.fields.items;
   console.log(props);
 
-  //   if (ds) {
-  //     return (
-  //       <div className={`component promo feature blog `} id="123">
-  //         <div className="component-content">
-  //           <div className="field-promoicon">
-  //             <JssImage field={ds.PromoIcon.jsonValue} />
-  //           </div>
-  //           <div className="promo-text">
-  //             <div>
-  //               <div className="field-promotext">
-  //                 <JssRichText field={ds.PromoText.jsonValue} />
-  //               </div>
-  //             </div>
-  //           </div>
-  //         </div>
-  //       </div>
-  //     );
-  //   }
+  if (ds) {
+    return (
+      <>
+        <h1>Featured Blogs</h1>
+        {ds.map((item) => {
+          return (
+            <div className={`component promo feature blog `} key={item.id}>
+              <div className="component-content">
+                <div className="promo-text">
+                  <div className="field-promotext">
+                    <h2>{item.name}</h2>
+                    <Text field={item.fields.Title} />
+                    <Text field={item.fields.Description} />
+                    <Text field={item.fields.SubTitle} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </>
+    );
+  }
 
   return <FeatureBlogDefaultComponent />;
 };
